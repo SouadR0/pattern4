5a7dd3a24a73d76eea8404817e39b6c0b0701a8d
Layanhani <145140373+Layanhani@users.noreply.github.com>
2024-05-09 15:29:06 +0300

    Update Office.java

--- Office.java
+++ Office.java
@@ -1,26 +1,72 @@
 
-package com.mycompany.company.system;
 
+
+//LAYAN ALNOUMANII
+
+
+package com.mycompany.company.system;
 import java.util.ArrayList;
 
 
-public class Office {
-    
-    public String number;
-    public ArrayList<Employee> employees;
-    public Employee head;
 
-    public Office(String number, Employee head) {
-        this.number = number;
-        this.head = head;
-        this.employees = new ArrayList<>();
-    }
+
+
+
+public class Office
+    {
+
+       //public vars
+        public String number;
+        public ArrayList<Employee> employees;
+        public Employee head;
+  
+
+
+        
+        
+        
+        
+        public Office(String number, Employee head)
+        {
+        
+
+
+//the same ones
+                  this.number = number;
+
+             this.head = head;
+
+
+
+              this.employees = new ArrayList<>();
+
+
+
+
+            }
+
+
+
+  public void addEmployee(Employee emp)
+        {
+            //add the employee name 
+         this.employees.add(emp);
+ }
+   
+   
     
-    public void addEmployee(Employee emp) {
-        this.employees.add(emp);
-    }
-    
-    public void removeEmployee(Employee emp) {
-        this.employees.remove(emp);
-    }
-}
+
+
+
+            public void removeEmployee(Employee emp)
+        {
+//remove the employee name 
+
+              this.employees.remove(emp);
+
+
+              }
+
+
+
+        }
