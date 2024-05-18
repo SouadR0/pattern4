b10b917b55c094fa51bc51caba6f6962c8a923dc
SouadR0 <131975482+SouadR0@users.noreply.github.com>
2024-05-18 21:47:12 +0300

    Update Company.java

--- Company.java
+++ Company.java
@@ -2,45 +2,57 @@ package com.mycompany.company.system;
 
 import java.util.ArrayList;
 
+// singltone and observer pattern
 
-public class Company implements Subject{
-public String name;
-    public Employee head;
-    ArrayList<Department> departments;
-    ArrayList<Observer> observers;
-    
- public Company(String name, Employee head) {
+public class Company implements Subject {
+    private static Company company;
+
+     String name;
+    private Employee head;
+    private ArrayList<Department> departments;
+    private ArrayList<Observer> observers;
+
+    /** Private constructor to prevent constructing companies from other classes. */
+    private Company(String name, Employee head) {
         this.name = name;
         this.head = head;
         this.departments = new ArrayList<>();
         this.observers = new ArrayList<>();
     }
-public void addDepartment(Department department) {
+
+    public static Company getCompany(String name, Employee head) {
+        if (Company.company == null) {
+            Company.company = new Company(name, head);
+        }
+        return Company.company;
+    }
+
+    public void addDepartment(Department department) {
         this.departments.add(department);
         this.addObserver(department);
-        
         this.notifyObservers();
     }
-public void removeDepartment(Department department) {
+
+    public void removeDepartment(Department department) {
         this.departments.remove(department);
         this.removeObserver(department);
-        
         this.notifyObservers();
     }
+
     @Override
     public void addObserver(Observer observer) {
         this.observers.add(observer);
     }
-   @Override
+
+    @Override
     public void removeObserver(Observer observer) {
         this.observers.remove(observer);
     }
-     @Override
+
+    @Override
     public void notifyObservers() {
         for (Observer observer : observers) {
             observer.update(this);
         }
     }
 }
-
-
